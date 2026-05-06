1f597bb483d9e27447919e5613ae4e490b4d5ab5
nguyenngoctri2910-lgtm <nguyenngoctri2910@gmail.com>
2026-05-07 03:07:27 +0700

    Add baccarat intro lobby screen

--- src/app/entries/training.js
+++ src/app/entries/training.js
@@ -1,9 +1,34 @@
 import "../../shared/services/auth-store.js";
 import { init } from "../../features/training/training-controller.js";
 
+function initLobbyIntro() {
+  const intro = document.getElementById("tr-lobby-intro");
+  const playButton = document.getElementById("trLobbyPlayBtn");
+  const hallButton = document.getElementById("trLobbyHallBtn");
+
+  if (!intro || !playButton) return;
+
+  document.body.classList.add("tr-lobby-is-open");
+
+  playButton.addEventListener("click", function () {
+    intro.classList.add("is-leaving");
+    document.body.classList.remove("tr-lobby-is-open");
+    window.setTimeout(function () {
+      intro.hidden = true;
+    }, 480);
+  });
+
+  if (hallButton) {
+    hallButton.addEventListener("click", function () {
+      window.location.href = "../home.html";
+    });
+  }
+}
+
 const _auth = window.YiDingAuthStore;
 if (_auth && !_auth.getSession()) {
   window.location.replace("../../index.html");
 } else {
   init();
+  initLobbyIntro();
 }
